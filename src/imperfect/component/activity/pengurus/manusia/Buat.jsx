0417ddcc2e918cd50da1fbb6a6dc6703ce4cf31a
Unknown <gilangpratamaw@gmail.com>
2018-01-04 12:45:03 +0700

    push

---
+++ src/imperfect/component/activity/pengurus/manusia/Buat.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from "react"
+import VariabelForm from "./VariabelForm";
+export default class Buat extends Component {
+  render() {
+    return (
+      <article className="post">
+        <p>Buat Menu.</p>
+        <VariabelForm/>
+      </article>
+    )
+  }
+};
---
+++ src/imperfect/component/activity/pengurus/manusia/Index.jsx
@@ -0,0 +1,41 @@
+import React,{Component} from "react"
+import TableList from "../../../list/TableList";
+import {Link} from "react-router-dom";
+import ApiHelper from "../../../../../json/ApiHelper";
+import {Observer} from "mobx-react";
+import mobxStore from "../../../../../mobx/mobxStore";
+class Index extends Component {
+  componentWillMount(){
+    this.loadData();
+  }
+  loadData(){
+    ApiHelper.pengurusVariabel();
+  }
+  render() {
+    return (
+      <article className="post">
+        <p>Variabel.</p>
+        <Link
+          className='button'
+          to={'/pengurus/variabel/buat'}
+        >Buat Variabel</Link>
+        <article className="post">
+          <Observer>
+            {()=>{
+              return(
+                <TableList
+                  pk={'nama'}
+                  klikHapus={(pk)=>{
+                    ApiHelper.pengurusVariabelHapus(pk)
+                  }}
+                  data={mobxStore.pengurusVariabel}
+                />
+              )
+            }}
+          </Observer>
+        </article>
+      </article>
+    )
+  }
+}
+export default Index
